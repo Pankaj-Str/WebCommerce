761495f50dbec1ab175310ca5bbd22be73b73653
Eisner López Acevedo <elopeza@ucatolica.ac.cr>
2018-07-28 11:14:56 -0600

    javascript

--- WebCommerce/settings.py
+++ WebCommerce/settings.py
@@ -118,6 +118,6 @@ STATIC_URL = '/static/'
 if DEBUG:
     MEDIA_URL = '/media/'
     STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "/static/", "static-only")
-    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
-    STATICFILES_DIRS = (os.path.dirname(BASE_DIR), "static", "static")
+    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "/static'", "media")
+    STATICFILES_DIRS = (os.path.dirname(BASE_DIR), "/static/", "static")
 
