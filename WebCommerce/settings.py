007a8bdd0fab29fc01c493e015f49569e95c789f
Eisner López Acevedo <elopeza@ucatolica.ac.cr>
2018-07-30 12:09:14 -0600

    LOGIN EMAIL
    AUTH

--- WebCommerce/settings.py
+++ WebCommerce/settings.py
@@ -42,7 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles.apps.ProfilesConfig',
+
+    # CRISPY
     'crispy_forms',
+    # END OF CRISPY
+
+    # pip install django-allauth
+
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -67,6 +73,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.vimeo',
     'allauth.socialaccount.providers.weibo',
     'allauth.socialaccount.providers.xing',
+    # END OF ALLAUTH
 ]
 
 MIDDLEWARE = [
@@ -89,8 +96,10 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                # REQUIRED FOR django-allauth
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                # django-allauth NEEDS THIS FROM DJANGO
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -98,6 +107,7 @@ TEMPLATES = [
     },
 ]
 
+# ADD THIS FOR django-allauth
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
@@ -105,6 +115,7 @@ AUTHENTICATION_BACKENDS = (
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 )
+# END OF REQUIREMENTS FOR django-allauth
 
 WSGI_APPLICATION = 'WebCommerce.wsgi.application'
 
@@ -160,6 +171,7 @@ if DEBUG:
     MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "/static'", "media")
     STATICFILES_DIRS = (os.path.dirname(BASE_DIR), "/static/", "static")
 
+# pip install --upgrade django-crispy-forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 SITE_ID = 1
@@ -168,3 +180,27 @@ LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/'
 
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+ACCOUNT_CONFIRM_EMAIL_ON_GET = False
+ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = LOGIN_URL
+ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = None
+
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 3
+ACCOUNT_EMAIL_REQUIRED = False
+ACCOUNT_EMAIL_VERIFICATION = None
+ACCOUNT_EMAIL_SUBJECT_PREFIX = 'My Subject: '
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "http"
+
+ACCOUNT_LOGOUT_ON_GET = False
+ACCOUNT_LOGOUT_REDIRECT_URL = "/"
+ACCOUNT_SIGNUP_FORM_CLASS = None
+ACCOUNT_SIGNUP_PASSWORD_VERIFICATION = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_USER_MODEL_USERNAME_FIELD = "username"
+ACCOUNT_USER_MODEL_EMAIL_FIELD = "email"
+
+ACCOUNT_USERNAME_MIN_LENGTH = 5
+ACCOUNT_USERNAME_BLACKLIST = []
+ACCOUNT_USERNAME_REQUIRED = True
+ACCOUNT_PASSWORD_INPUT_RENDER_VALUE = False
+ACCOUNT_PASSWORD_MIN_LENGTH = 6
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
